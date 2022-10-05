c4063c56a696920a8fa72da5e0daac51523adb0d
Rofai <66499186+ngarofad@users.noreply.github.com>
2022-10-05 20:05:46 +0700

    Add files via upload

---
+++ kamus_stopword.py
@@ -0,0 +1,108 @@
+
+
+kamus_stopword = [
+    'aku',
+    'saya',
+    'daku',
+    'kita',
+    'kami',
+    #
+    'kamu',
+    'anda',
+    'kau',
+    'kalian',
+    #
+    'ia',
+    'dia',
+    'beliau',
+    'mereka',
+    #
+    'ku',
+    'mu',
+    'nya',
+    'mereka',
+    #
+    'yang',
+    #
+    'ini',
+    'itu',
+    'sana',
+    'sini',
+    'situ',
+    #
+    'ke',
+    'di',
+    #
+    'untuk',
+    'pada',
+    'ke',
+    #
+    # 'sesuatu',
+    'seseorang',
+    'para',
+    #
+    # # konjungsi
+    #
+    'dan',
+    'serta',
+    'beserta',
+    #
+    'tetapi',
+    'melainkan',
+    #
+    'atau',
+    #
+
+    #
+    # # Provider
+    #
+    'indihome',
+    'firstmedia',
+    'biznet',
+    'xl',
+    'indosat',
+    'tri',
+    'telkomsel',
+    'tsel',
+    'usee',
+    'useetv',
+    'go',
+    'first',
+    'media',
+    'telkom',
+
+    #
+    'adalah',
+    #
+    'admin',
+    'kakak',
+    'kak',
+    'pak',
+    'bapak',
+    'bu',
+    #
+    'id',
+    #
+    'amp',
+    #
+    'tanggal',
+    'hari',
+    'bulan',
+    'tahun',
+    'senin',
+    'selasa',
+    'rabu',
+    'kamis',
+    'jumat',
+    'sabtu',
+    'minggu',
+    #
+    'kan',
+    'nya',
+    #
+    'nomer',
+    'nomor',
+]
+
+# print(type(kamus_stopword))
+# print(kamus_stopword)
